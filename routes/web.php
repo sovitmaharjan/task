23c13895ce2e09ffcbbee7a8add9bcc7c945c0ab
sovitmaharjan <sovitmaharjan@gmail.com>
2024-05-22 19:41:37 +0545

    commit test 4

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,3 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.index');
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// commit alias test 3
